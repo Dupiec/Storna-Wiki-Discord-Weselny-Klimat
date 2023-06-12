f204a26242443cee19b1aa295646c23de5d36c5e
Dupiec <135479783+Dupiec@users.noreply.github.com>
2023-06-12 02:44:14 +0200

    UPDATE ALFA
    SMIESZNY OPIS

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("button");
+button.addEventListener("click", () => {
+  window.open("https://www.youtube.com/watch?v=BP1UTQXoAsk");
+  // Kod obsługi zdarzenia po kliknięciu przycisku
+});
