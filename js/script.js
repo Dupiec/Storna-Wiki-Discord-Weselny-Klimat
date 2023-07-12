6fe138c1a1c9218f297d846a88b482d422aa8161
MichałKostrz55 <135479783+MichalKostrz55@users.noreply.github.com>
2023-07-13 00:50:29 +0200

    Remont

--- js/script.js
+++
@@ -1,5 +0,0 @@
-const button = document.getElementById("button");
-button.addEventListener("click", () => {
-  window.open("https://www.youtube.com/watch?v=BP1UTQXoAsk");
-  // Kod obsługi zdarzenia po kliknięciu przycisku
-});
